6c7da89e9e479054878744981f1bd195c3b85c55
CiprianaM <cipriana2001@gmail.com>
2020-04-23 15:27:14 +0200

    implement router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Link, Route} from "react-router-dom";
 import './index.css';
-import App from './App';
+import AppWithHooks from './AppWithHooks';
+import AppWithoutHooks from './AppWithoutHooks'
 import * as serviceWorker from './serviceWorker';
+import store from './redux';
+import {Provider} from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={store} >
+        <Link to="/appwithhooks">
+          <header>App With Hooks</header>
+        </Link>
+        <Link to="/appwithouthooks">
+          <header>App Without Hooks</header>
+        </Link>
+        <Switch>
+          <Route path="/appwithhooks">
+            <AppWithHooks />
+          </Route>
+          <Route path="/appwithouthooks">
+            <AppWithoutHooks />
+          </Route>
+
+        </Switch>
+      </Provider>
+      </Router>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
